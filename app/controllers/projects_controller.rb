5019113671c0021e5041b8281bc5494d03bbd1c5
Toby Retallick <toby.retallick@gmail.com>
2017-09-04 00:07:49 +0100

    Handle cases where project cannot be found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -8,7 +10,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been destroyed."
@@ -17,11 +18,9 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated"
       redirect_to @project
@@ -39,18 +38,23 @@ class ProjectsController < ApplicationController
      redirect_to @project
    else
      flash[:alert] = "Project has not been created."
-
      render "new"
-   end  
+   end
   end
   
   def show
-    @project = Project.find(params[:id])
   end
 
   private
 
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found"
+    redirect_to projects_path
   end
 end
