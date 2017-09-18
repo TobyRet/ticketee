da88fa843acdc5a79bc6d117fcdb8e18ea6019ce
Toby Retallick <toby.retallick@gmail.com>
2017-09-18 10:46:48 +0100

    Edit tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,8 +7,20 @@ class TicketsController < ApplicationController
   end
 
   def show
-    @ticket
   end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = 'Ticket has been updated'
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = 'Ticket has not been updated'
+      render 'edit'
+    end
+  end 
 
   def create
     @ticket = @project.tickets.build(ticket_params)
